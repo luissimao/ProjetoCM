2abaa45049af7a345ff41920a02206deba7b0595
rimorais <61158317+rimorais@users.noreply.github.com>
2021-06-12 22:26:50 +0100

    Alterado updateProduto e Criado endpoint createProduto

--- app/src/main/java/com/example/projetocm/api/EndPoints.kt
+++ app/src/main/java/com/example/projetocm/api/EndPoints.kt
@@ -13,12 +13,17 @@ interface EndPoints {
 
     @FormUrlEncoded
     @POST("eliminarProduto")
-    fun deleteProduto(@Field("id") id: Int?): Call<OutputPost>
+    fun deleteProduto(@Field("id") id: Int): Call<OutputPost>
 
     @FormUrlEncoded
     @POST("alterarProduto")
-    fun updateProduto(@Field("id") id: String?, @Field("titulo") titulo: String?,
-                  @Field("preco") preco: String?,
+    fun updateProduto(@Field("id") id: Int, @Field("titulo") titulo: String?,
+                  @Field("preco") preco: Float?, @Field("categoria") categoria: String?,
+                  @Field("stock") stock: Int?): Call<OutputPost>
+
+    @FormUrlEncoded
+    @POST("criarProduto")
+    fun createProduto(@Field("titulo") titulo: String?, @Field("preco") preco: String?,
                       @Field("categoria") categoria: String?,
                   @Field("stock") stock: String?): Call<OutputPost>
 
